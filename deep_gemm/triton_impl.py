3e4774f0484fbb119cb612c6261a5a366c4ac7c4
Tiancheng Chen <tiachen@student.ethz.ch>
2025-03-14 22:59:56 +0100

    fuse transpose kernel, improve perf

--- deep_gemm/triton_impl.py
+++ deep_gemm/triton_impl.py
@@ -9,26 +9,33 @@ def _per_token_quantize(
     x_ptr,
     x_fp8_ptr,
     x_sf_ptr,
-    num_rows,
+    m, # row
+    n, # column
+    m_stride, 
+    n_stride,
     eps,
     fp8_max,
     BLOCK_SIZE: tl.constexpr # number of 128-element blocks
 ):
-    pid = tl.program_id(0)
-     
-    off_r = tl.arange(0, BLOCK_SIZE) + pid * BLOCK_SIZE
-    off_c = tl.arange(0, BLOCK_SIZE)
-    offset = off_r[:, None] * BLOCK_SIZE + off_c[None, :]
-    mask = (off_r[:, None] < num_rows) & (off_c[None, :] < BLOCK_SIZE)
-    x = tl.load(x_ptr + offset, mask=mask, other=0.0)
+    # input x (m,n) can be row/column-major
+    # output x_fp8 (m,n) is row-major
+    # output x_sf (m,n//128) is column-major
     
+    pid_r = tl.program_id(0)
+    pid_c = tl.program_id(1)
+    
+    off_r = pid_r * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    off_c = pid_c * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    offset = off_r[:, None] * m_stride + off_c[None, :] * n_stride
+    mask = (off_r[:, None] < m) & (off_c[None, :] < n)
+    x = tl.load(x_ptr + offset, mask=mask, other=0.0)
     amax = tl.maximum(tl.max(tl.abs(x).to(tl.float32), axis=-1), eps)
     x_sf = (amax / fp8_max).reshape(BLOCK_SIZE)
     x_fp8 = (x * (fp8_max / amax).expand_dims(axis=1)).to(x_fp8_ptr.dtype.element_ty)
-
-    tl.store(x_fp8_ptr + offset, x_fp8, mask=mask)
-    
-    tl.store(x_sf_ptr + off_r, x_sf, mask=(off_r < num_rows))
+    offset_output = off_r[:, None] * n + off_c[None, :]
+    offset_sf = off_r + m * pid_c
+    tl.store(x_fp8_ptr + offset_output, x_fp8, mask=mask)
+    tl.store(x_sf_ptr + offset_sf, x_sf)
     
 def per_token_quantize(
     x: torch.Tensor,
@@ -36,70 +43,70 @@ def per_token_quantize(
     eps: float = 1e-4,
     dtype: torch.dtype = torch.float8_e4m3fn,
 ) -> Tuple[torch.Tensor, torch.Tensor]:    
+    # input x (m,n) can be row/column-major
+    # output x_fp8 (m,n) is row-major
+    # output x_sf (m,n//128) is column-major
     assert block_size == 128, 'Currently only support block_size=128'
-    assert x.dim() in [2, 3] and x.size(-1) % block_size == 0
-    assert x.is_contiguous()
-    
-    if x.dim() == 3:
-        mb_size = x.size(0)
-        seq_len = x.size(1)
-    else:
-        mb_size = None
-        seq_len = x.size(0)
-    
-    x = x.view(-1, block_size)
+    assert x.dim() == 2 and x.size(1) % block_size == 0
+    m, n = x.shape
+    m_stride, n_stride = x.stride(0), x.stride(1)
     
     finfo = torch.finfo(dtype)
     fp8_max = finfo.max
-    fp8_min = -fp8_max
     
-    x_fp8 = torch.empty(x.shape, dtype=dtype, device=x.device)
-    x_sf = torch.empty((x.numel()//block_size), dtype=torch.float32, device=x.device)
-    _per_token_quantize[(x.numel()//(block_size ** 2),)](
+    x_fp8 = torch.empty((m, n), dtype=dtype, device=x.device)
+    x_sf = torch.empty((n // block_size, m), dtype=torch.float32, device=x.device).transpose(0, 1)
+    _per_token_quantize[(m // block_size, n // block_size)](
         x,
         x_fp8,
         x_sf,
-        x.numel()//block_size,
+        m,
+        n,
+        m_stride,
+        n_stride,
         eps,
         fp8_max,
         block_size
     )
-    if mb_size is None:
-        return x_fp8.view(seq_len, -1), get_col_major_tma_aligned_tensor(x_sf.view(seq_len, -1))
-    else:
-        return x_fp8.view(mb_size, seq_len, -1), get_col_major_tma_aligned_tensor(x_sf.view(mb_size, seq_len, -1))
+    assert x_fp8.stride(0) == n and x_fp8.stride(1) == 1 # row-major
+    assert x_sf.stride(0) == 1 and x_sf.stride(1) == m # column-major
+    
+    return x_fp8, x_sf
 
 def transpose_per_token_quantize(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    assert x.is_contiguous()
-    x = x.transpose(-1, -2).contiguous()
-    return per_token_quantize(x)
-
+    assert x.dim() == 2
+    return per_token_quantize(x.transpose(0, 1))
 
 @triton.jit
 def _per_block_quantize(
     x_ptr,
     x_fp8_ptr,
     x_sf_ptr,
-    num_rows,
-    num_cols,
+    m, # row
+    n, # column
+    m_stride,
+    n_stride,
     eps,
-    fp8_min,
     fp8_max,
-    BLOCK_SIZE: tl.constexpr # number of 128x128-element blocks
+    BLOCK_SIZE: tl.constexpr
 ):
+    # input x (m,n) can be row/column-major
+    # output x_fp8 (m,n) is row-major
+    # output x_sf (m//128,n//128) is row-major
+    
     pid_r = tl.program_id(0)
     pid_c = tl.program_id(1)
     off_r = pid_r * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
     off_c = pid_c * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    offset = off_r[:, None] * num_cols + off_c[None, :]
-    mask = (off_r[:, None] < num_rows) & (off_c[None, :] < num_cols)
+    offset = off_r[:, None] * m_stride + off_c[None, :] * n_stride
+    mask = (off_r[:, None] < m) & (off_c[None, :] < n)
     x_tile = tl.load(x_ptr + offset, mask=mask, other=0.0)
     amax = tl.maximum(tl.max(tl.abs(x_tile).to(tl.float32)), eps)
     x_fp8 = (x_tile * (fp8_max / amax)).to(x_fp8_ptr.dtype.element_ty)
-    tl.store(x_fp8_ptr + offset, x_fp8, mask=mask)
-    
-    n_blocks_per_row = tl.cdiv(num_cols, BLOCK_SIZE)
-    tl.store(x_sf_ptr + pid_r * n_blocks_per_row + pid_c, amax / fp8_max)
+    offset_fp8 = off_r[:, None] * n + off_c[None, :]
+    offset_sf = pid_r * n // BLOCK_SIZE + pid_c
+    tl.store(x_fp8_ptr + offset_fp8, x_fp8, mask=mask)
+    tl.store(x_sf_ptr + offset_sf, amax / fp8_max)
 
 def per_block_quantize(
     x: torch.Tensor,
@@ -107,45 +114,38 @@ def per_block_quantize(
     eps: float = 1e-4,
     dtype: torch.dtype = torch.float8_e4m3fn,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
+    # input x (m,n) can be row/column-major
+    # output x_fp8 (m,n) is row-major
+    # output x_sf (m//128,n//128) is row-major
     assert block_size == 128, 'Currently only support block_size=128'
-    assert x.dim() in [2, 3] and x.size(-1) % block_size == 0
-    assert x.is_contiguous()
-    
-    if x.dim() == 3:
-        mb_size = x.size(0)
-        seq_len = x.size(1)
-    else:
-        mb_size = None
-        seq_len = x.size(0)
-    
-    x = x.view(-1, x.size(-1))
+    assert x.dim() == 2 and x.size(0) % block_size == 0 and x.size(1) % block_size == 0
+    m, n = x.shape
+    m_stride, n_stride = x.stride(0), x.stride(1)
     
     finfo = torch.finfo(dtype)
     fp8_max = finfo.max
-    fp8_min = -fp8_max
     
-    x_fp8 = torch.empty(x.shape, dtype=dtype, device=x.device)
-    x_sf = torch.empty((x.numel() // (block_size ** 2)), dtype=torch.float32, device=x.device)
-    _per_block_quantize[(x.size(0) // block_size, x.size(1) // block_size)](
+    x_fp8 = torch.empty((m, n), dtype=dtype, device=x.device)
+    x_sf = torch.empty((m // block_size, n // block_size), dtype=torch.float32, device=x.device)
+    _per_block_quantize[(m // block_size, n // block_size)](
         x,
         x_fp8,
         x_sf,
-        x.size(0),
-        x.size(1),
+        m,
+        n,
+        m_stride,
+        n_stride,
         eps,
-        fp8_min,
         fp8_max,
         block_size
     )
-    if mb_size is None:
-        return x_fp8.view(seq_len, -1), x_sf.view(seq_len // block_size, -1)
-    else:
-        return x_fp8.view(mb_size, seq_len, -1), x_sf.view(mb_size, seq_len // block_size, -1)
+    assert x_fp8.stride(0) == n and x_fp8.stride(1) == 1 # row-major
+    assert x_sf.stride(0) == n // block_size and x_sf.stride(1) == 1 # row-major
+    return x_fp8, x_sf
     
 def transpose_per_block_quantize(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    assert x.is_contiguous()
-    x = x.transpose(-1, -2).contiguous()
-    return per_block_quantize(x)
+    assert x.dim() == 2
+    return per_block_quantize(x.transpose(0, 1))
 
 
 def print_to_csv_file(x: torch.Tensor, name: str):
@@ -189,17 +189,36 @@ def test_per_token_quantize():
     sf_max_err = torch.max(torch.abs(x_sf.to(torch.float32) - ref_x_sf.to(torch.float32)))
     print(f'per_token_quantize x_max_err={x_max_err:.4f}, sf_max_err={sf_max_err:.4f}')
 
+def test_transpose_per_token_quantize():
+    
+    x = torch.randn(1024, 1024, device='cuda', dtype=torch.bfloat16)
+    x_fp8, x_sf = transpose_per_token_quantize(x)
+    assert x_fp8.shape == (1024, 1024), f'{x_fp8.shape=}'
+    assert x_sf.shape == (1024, 8), f'{x_sf.shape=}'
+    ref_x_fp8, ref_x_sf = per_token_cast_to_fp8(x.transpose(0, 1).contiguous())
+    x_max_err = torch.max(torch.abs(x_fp8.to(torch.float32) - ref_x_fp8.to(torch.float32)))
+    sf_max_err = torch.max(torch.abs(x_sf.to(torch.float32) - ref_x_sf.to(torch.float32)))
+    print(f'transpose_per_token_quantize x_max_err={x_max_err:.4f}, sf_max_err={sf_max_err:.4f}')
+
 def test_per_block_quantize():
     x = torch.randn(1024, 1024, device='cuda', dtype=torch.bfloat16)
     x_fp8, x_sf = per_block_quantize(x)
     assert x_fp8.shape == (1024, 1024), f'{x_fp8.shape=}'
     assert x_sf.shape == (8, 8), f'{x_sf.shape=}'
     ref_x_fp8, ref_x_sf = per_block_cast_to_fp8(x)
-    # print_to_csv_file(x_fp8, 'x_fp8.csv')
-    # print_to_csv_file(ref_x_fp8, 'ref_x_fp8.csv')
     x_max_err = torch.max(torch.abs(x_fp8.to(torch.float32) - ref_x_fp8.to(torch.float32)))
     sf_max_err = torch.max(torch.abs(x_sf.to(torch.float32) - ref_x_sf.to(torch.float32)))
     print(f'per_block_quantize x_max_err={x_max_err:.4f}, sf_max_err={sf_max_err:.4f}')
+    
+def test_transpose_per_block_quantize():
+    x = torch.randn(1024, 1024, device='cuda', dtype=torch.bfloat16)
+    x_fp8, x_sf = transpose_per_block_quantize(x)
+    assert x_fp8.shape == (1024, 1024), f'{x_fp8.shape=}'
+    assert x_sf.shape == (8, 8), f'{x_sf.shape=}'
+    ref_x_fp8, ref_x_sf = per_block_cast_to_fp8(x.transpose(0, 1).contiguous())
+    x_max_err = torch.max(torch.abs(x_fp8.to(torch.float32) - ref_x_fp8.to(torch.float32)))
+    sf_max_err = torch.max(torch.abs(x_sf.to(torch.float32) - ref_x_sf.to(torch.float32)))
+    print(f'transpose_per_block_quantize x_max_err={x_max_err:.4f}, sf_max_err={sf_max_err:.4f}')
 
 def benchmark():
     size = [2 ** i for i in range(10, 15)]
@@ -224,7 +243,33 @@ def benchmark():
             torch.cuda.synchronize()
             return t0.elapsed_time(t1) / 32
 
-        print(f'{s=}, {bench(per_token_quantize, x)=:.4f}, {bench(per_block_quantize, x)=:.4f}, {bench(per_token_cast_to_fp8, x)=:.4f}, {bench(per_block_cast_to_fp8, x)=:.4f}')
+        print(f'{s=}, triton_per_token: {bench(lambda x: per_token_quantize(x), x):.4f}, torch_per_token: {bench(lambda x: per_token_cast_to_fp8(x), x):.4f}, triton_per_block: {bench(lambda x: per_block_quantize(x), x):.4f}, torch_per_block: {bench(lambda x: per_block_cast_to_fp8(x), x):.4f}')
+        
+
+def benchmark_transpose():
+    size = [2 ** i for i in range(10, 15)]
+    for s in size:
+        x = torch.randn(s, s, device='cuda', dtype=torch.bfloat16)
+        
+        for _ in range(2):
+            # warmup
+            transpose_per_token_quantize(x)
+            transpose_per_block_quantize(x)
+            per_token_cast_to_fp8(x.transpose(0, 1).contiguous())
+            per_block_cast_to_fp8(x.transpose(0, 1).contiguous())
+        
+        def bench(func, x):
+            t0 = torch.cuda.Event(enable_timing=True)
+            t1 = torch.cuda.Event(enable_timing=True)
+            torch.cuda.synchronize()
+            t0.record()
+            for _ in range(32):
+                func(x)
+            t1.record()
+            torch.cuda.synchronize()
+            return t0.elapsed_time(t1) / 32
+
+        print(f'{s=}, triton_per_token: {bench(lambda x: transpose_per_token_quantize(x), x):.4f}, torch_per_token: {bench(lambda x: per_token_cast_to_fp8(x.transpose(0, 1).contiguous()), x):.4f}, triton_per_block: {bench(lambda x: transpose_per_block_quantize(x), x):.4f}, torch_per_block: {bench(lambda x: per_block_cast_to_fp8(x.transpose(0, 1).contiguous()), x):.4f}')
         
         
 
@@ -232,7 +277,11 @@ def benchmark():
 if __name__ == "__main__":
 
         
-    # test_per_token_quantize()
-    # test_per_block_quantize()
+    test_per_token_quantize()
+    test_per_block_quantize()
+    test_transpose_per_token_quantize()
+    test_transpose_per_block_quantize()
+    
     benchmark()
+    benchmark_transpose()
     
